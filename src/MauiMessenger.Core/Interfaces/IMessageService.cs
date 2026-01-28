ce328a9b936d5c03d19ca8212fef1e91f55703ee
Ben Underwood <javerikr@gmail.com>
2026-01-28 13:38:09 -0500

    Delete gitignore changes

--- src/MauiMessenger.Core/Interfaces/IMessageService.cs
+++
@@ -1,9 +0,0 @@
-using MauiMessenger.Core.DTOs;
-
-namespace MauiMessenger.Core.Interfaces;
-
-public interface IMessageService
-{
-    Task<MessageDto> CreateAsync(CreateMessageRequest request, CancellationToken cancellationToken = default);
-    Task<IReadOnlyList<MessageDto>> ListByConversationIdAsync(Guid conversationId, CancellationToken cancellationToken = default);
-}
--- tests/MauiMessenger.Client.Web.Tests/Playwright/WebAppCollection.cs
+++
@@ -1,6 +0,0 @@
-namespace MauiMessenger.Client.Web.Tests.Playwright;
-
-[CollectionDefinition("WebApp collection")]
-public sealed class WebAppCollection : ICollectionFixture<WebAppFixture>
-{
-}
